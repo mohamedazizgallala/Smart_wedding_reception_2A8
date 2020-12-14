d748c9540c77c2fc4940559b098c919a2088d351
zied <zied.soukni@esprit.tn>
2020-12-14 23:52:13 +0100

    commit 3

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,9 +13,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->tableView_2->setModel(tmpev.afficher());
+    ui->tableView_2_mar->setModel(tmpev.afficher());
     evenement e;
-    ui->tableView_2->setModel(e.afficher());
+    ui->tableView_2_mar->setModel(e.afficher());
     this->ui->stackedWidget->setCurrentIndex(0);
 
 }
@@ -28,86 +28,86 @@ MainWindow::~MainWindow()
 
 
 
-void MainWindow::on_buton_ajouter_clicked()
+void MainWindow::on_buton_ajouter_mar_clicked()
 {
 this->ui->stackedWidget->setCurrentIndex(1);
 }
 
-void MainWindow::on_pushButton_4_clicked()
+void MainWindow::on_pushButton_4_mar_clicked()
 {
 
     offre o;
-    this->ui->tableView->setModel(o.afficher());
+    this->ui->tableView_mar->setModel(o.afficher());
     this->ui->stackedWidget->setCurrentIndex(2) ;
 }
 
-void MainWindow::on_boutonsupprimer_clicked()
+void MainWindow::on_boutonsupprimer_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(3) ;
 }
 
-void MainWindow::on_bouton_modifier_clicked()
+void MainWindow::on_bouton_modifier_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(4) ;
 }
 
-void MainWindow::on_retour4_clicked()
+void MainWindow::on_retour4_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(0);
 }
 
-void MainWindow::on_pushButton_2_clicked()
+void MainWindow::on_pushButton_2_mar_clicked()
 {
 close();
         }
 
-void MainWindow::on_RETOUR1_clicked()
+void MainWindow::on_RETOUR1_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(0);
 }
 
-void MainWindow::on_pushButton_3_clicked()
+void MainWindow::on_pushButton_3_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(7);
 }
 
-void MainWindow::on_lineEdit_textChanged(const QString &arg1)
+void MainWindow::on_lineEdit_mar_textChanged(const QString &arg1)
 {
     offre o;
     if (arg1=="")
-    this->ui->tableView->setModel(o.afficher());
+    this->ui->tableView_mar->setModel(o.afficher());
     else
-    this->ui->tableView->setModel(o.rechercher2(arg1));
+    this->ui->tableView_mar->setModel(o.rechercher2(arg1));
 
 
 }
 
-void MainWindow::on_pushButton_5_clicked()
+void MainWindow::on_pushButton_5_mar_clicked()
 {
-     int id=ui->id_ajout->text().toInt();
-    offre o(id,this->ui->nom_ajout->text(),this->ui->dated_ajout->text(),this->ui->datef_ajout->text());
+     int id=ui->id_ajout_mar->text().toInt();
+    offre o(id,this->ui->nom_ajout_mar->text(),this->ui->dated_ajout_mar->text(),this->ui->datef_ajout_mar->text());
     if(o.ajouter())
     {
-      ui->tableView->setModel(o.afficher());
+      ui->tableView_mar->setModel(o.afficher());
       MainWindow::notif("Ajout reussi","Ajout reussi");
 }}
 
-void MainWindow::on_SUPPRIMER_clicked()
+void MainWindow::on_SUPPRIMER_mar_clicked()
 { offre o;
-    QString id=ui->id_supp->text();
+    QString id=ui->id_supp_mar->text();
     if(o.supprimer(id))
-    {this->ui->tableView->setModel(o.afficher());
+    {this->ui->tableView_mar->setModel(o.afficher());
         this->ui->stackedWidget->setCurrentIndex(1) ;
      MainWindow::notif("supression reussite","supression reussite");}
 }
 
-void MainWindow::on_MODIF_clicked()
+void MainWindow::on_MODIF_mar_clicked()
 {
-    int id=ui->id_Modif->text().toInt();
-   offre o(id,this->ui->nom_Modif->text(),this->ui->dated_Modif->text(),this->ui->datef_Modif->text());
+    int id=ui->id_Modif_mar->text().toInt();
+   offre o(id,this->ui->nom_Modif_mar->text(),this->ui->dated_Modif_mar->text(),this->ui->datef_Modif_mar->text());
    if(o.modifier())
    {this->ui->stackedWidget->setCurrentIndex(1);
-     ui->tableView->setModel(o.afficher());
+     ui->tableView_mar->setModel(o.afficher());
      MainWindow::notif("Modification reussie","Modification reussie");
 
    }
@@ -126,112 +126,112 @@ void MainWindow::notif(QString t,QString m)
 
 
 
-void MainWindow::on_pushButton_8_clicked()
+void MainWindow::on_pushButton_8_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(6);
 }
 
-void MainWindow::on_eventsupprimer_2_clicked()
+void MainWindow::on_eventsupprimer_2_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(10);
 }
 
-void MainWindow::on_retour4_2_clicked()
+void MainWindow::on_retour4_2_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(7);
 }
 
-void MainWindow::on_pushButton_11_clicked()
+void MainWindow::on_pushButton_11_mar_clicked()
 {
    this->ui->stackedWidget->setCurrentIndex(7);
 }
 
-void MainWindow::on_pushButton_12_clicked()
+void MainWindow::on_pushButton_12_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(9);
 }
 
-void MainWindow::on_RETOUR1_3_clicked()
+void MainWindow::on_RETOUR1_3_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(7);
 }
 
-void MainWindow::on_retour4_5_clicked()
+void MainWindow::on_retour4_5_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(7);
 }
 
-void MainWindow::on_event_ajouter_2_clicked()
+void MainWindow::on_event_ajouter_2_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(8);
 }
 
-void MainWindow::on_event_afficher_clicked()
+void MainWindow::on_event_afficher_mar_clicked()
 {
     evenement e;
     this->ui->stackedWidget->setCurrentIndex(9);
-     ui->tableView_2->setModel(e.afficher());
+     ui->tableView_2_mar->setModel(e.afficher());
 }
 
-void MainWindow::on_event_modifier_2_clicked()
+void MainWindow::on_event_modifier_2_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(11);
 }
 
-void MainWindow::on_pushButton_9_clicked()
+void MainWindow::on_pushButton_9_mar_clicked()
 {
 
-    int id=ui->id_ajout_2->text().toInt();
-   evenement e(id,this->ui->nom_ajout_2->text(),this->ui->date_ajout_2->text());
+    int id=ui->id_ajout_2_mar->text().toInt();
+   evenement e(id,this->ui->nom_ajout_2_mar->text(),this->ui->date_ajout_2_mar->text());
    if(e.ajouter())
    {
-     ui->tableView_2->setModel(e.afficher());
+     ui->tableView_2_mar->setModel(e.afficher());
      MainWindow::notif("Ajout reussi","Ajout reussi");
 }
 
 }
 
-void MainWindow::on_lineEdit_2_textChanged(const QString &arg1)
+void MainWindow::on_lineEdit_2_mar_textChanged(const QString &arg1)
 {
     evenement e;
     if (arg1=="")
-    this->ui->tableView_2->setModel(e.afficher());
+    this->ui->tableView_2_mar->setModel(e.afficher());
     else
-    this->ui->tableView_2->setModel(e.rechercher2(arg1));
+    this->ui->tableView_2_mar->setModel(e.rechercher2(arg1));
 }
 
-void MainWindow::on_SUPPRIMER_3_clicked()
+void MainWindow::on_SUPPRIMER_3_mar_clicked()
 {
 
     evenement e;
-        QString id=ui->id_supp_3->text();
+        QString id=ui->id_supp_3_mar->text();
         if(e.supprimer(id))
-        {this->ui->tableView_2->setModel(e.afficher());
+        {this->ui->tableView_2_mar->setModel(e.afficher());
             this->ui->stackedWidget->setCurrentIndex(8) ;
          MainWindow::notif("supression reussite","supression reussite");}
 
 }
 
-void MainWindow::on_pushButton_24_clicked()
+void MainWindow::on_pushButton_24_mar_clicked()
 {
-    int id=ui->id_ajout_5->text().toInt();
-  evenement e(id,this->ui->nom_ajout_5->text(),this->ui->date_ajout_4->text());
+    int id=ui->id_ajout_5_mar->text().toInt();
+  evenement e(id,this->ui->nom_ajout_5_mar->text(),this->ui->date_ajout_4_mar->text());
    if(e.modifier())
    {
-     ui->tableView_2->setModel(e.afficher());}
+     ui->tableView_2_mar->setModel(e.afficher());}
 this->ui->stackedWidget->setCurrentIndex(8);
 MainWindow::notif("Modification reussie","Modification reussie");
 
 }
 
-void MainWindow::on_pushButton_7_clicked()
+void MainWindow::on_pushButton_7_mar_clicked()
 {
      this->ui->stackedWidget->setCurrentIndex(8);
 }
 
 
 
-void MainWindow::on_pushButton_13_clicked()
+void MainWindow::on_pushButton_13_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(1);
 }
@@ -277,19 +277,19 @@ void MainWindow::on_pushButton_13_clicked()
 
 
 
-void MainWindow::on_pushButton_65_clicked()
+void MainWindow::on_pushButton_65_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(6);
 
 }
 
-void MainWindow::on_pushButton_14_clicked()
+void MainWindow::on_pushButton_14_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(1);
 
 }
 
-void MainWindow::on_retour4_3_clicked()
+void MainWindow::on_retour4_3_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(7);
 
@@ -297,13 +297,13 @@ void MainWindow::on_retour4_3_clicked()
 
 
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_pushButton_mar_clicked()
 {
     QString strStream;
                 QTextStream out(&strStream);
 
-                const int rowCount = ui->tableView_2->model()->rowCount();
-                const int columnCount = ui->tableView_2->model()->columnCount();
+                const int rowCount = ui->tableView_2_mar->model()->rowCount();
+                const int columnCount = ui->tableView_2_mar->model()->columnCount();
                 QString TT = QDate::currentDate().toString("dd/mm/yyyy");
                 out <<"<html>\n"
                       "<head>\n"
@@ -317,16 +317,16 @@ void MainWindow::on_pushButton_clicked()
                 // headers
                 out << "<thead><tr bgcolor=#d6e5ff>";
                 for (int column = 0; column < columnCount; column++)
-                    if (!ui->tableView_2->isColumnHidden(column))
-                        out << QString("<th>%1</th>").arg(ui->tableView_2->model()->headerData(column, Qt::Horizontal).toString());
+                    if (!ui->tableView_2_mar->isColumnHidden(column))
+                        out << QString("<th>%1</th>").arg(ui->tableView_2_mar->model()->headerData(column, Qt::Horizontal).toString());
                 out << "</tr></thead>\n";
 
                 // data table
                 for (int row = 0; row < rowCount; row++) {
                     out << "<tr>";
                     for (int column = 0; column < columnCount; column++) {
-                        if (!ui->tableView_2->isColumnHidden(column)) {
-                            QString data =ui->tableView_2->model()->data(ui->tableView_2->model()->index(row, column)).toString().simplified();
+                        if (!ui->tableView_2_mar->isColumnHidden(column)) {
+                            QString data =ui->tableView_2_mar->model()->data(ui->tableView_2_mar->model()->index(row, column)).toString().simplified();
                             out << QString("<td bkcolor=0>%1</td>").arg((!data.isEmpty()) ? data : QString("&nbsp;"));
                         }
                     }
@@ -353,25 +353,25 @@ void MainWindow::on_pushButton_clicked()
 
 
 
-void MainWindow::on_pushButton_10_clicked()
+void MainWindow::on_pushButton_10_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(7);
 
 }
 
-void MainWindow::on_pushButton_15_clicked()
+void MainWindow::on_pushButton_15_mar_clicked()
 {
     close();
 }
 
 
-void MainWindow::on_SUPPRIMER_2_clicked()
+void MainWindow::on_SUPPRIMER_2_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(5);
 
 }
 
-void MainWindow::on_SUPPRIMER_4_clicked()
+void MainWindow::on_SUPPRIMER_4_mar_clicked()
 {
     this->ui->stackedWidget->setCurrentIndex(9);
 
@@ -379,74 +379,74 @@ void MainWindow::on_SUPPRIMER_4_clicked()
 
 
 
-void MainWindow::on_tri_clicked()
+void MainWindow::on_tri_mar_clicked()
 {
     offre o;
        /*QString critere=ui->cb_historique->currentText();*/
            QString mode;
-            if (ui->rb_asc_historique->isChecked()==true)
+            if (ui->rb_asc_historique_mar->isChecked()==true)
        {
-                ui->tableView->setModel(o.trie());
+                ui->tableView_mar->setModel(o.trie());
 
 
        }
-            else if(ui->rb_desc_historique->isChecked()==true)
+            else if(ui->rb_desc_historique_mar->isChecked()==true)
 
-                ui->tableView->setModel(o.trie2());
+                ui->tableView_mar->setModel(o.trie2());
 }
 
-void MainWindow::on_recherche_clicked()
+void MainWindow::on_recherche_mar_clicked()
 {
     offre P;
        QString text;
 
-       if (ui->rid->isChecked()==true)
+       if (ui->rid_mar->isChecked()==true)
       {
-      text=ui->rech->text();
+      text=ui->rech_mar->text();
         if(text == "")
         {
-            ui->tableView->setModel(P.afficher());
+            ui->tableView_mar->setModel(P.afficher());
         }
 
         else
         {
-            ui->tableView->setModel(P.chercher_offre_id(text));
+            ui->tableView_mar->setModel(P.chercher_offre_id(text));
         }
        }
 
 
-      if(ui->rnom->isChecked()==true)
+      if(ui->rnom_mar->isChecked()==true)
        {
-           text=ui->rech->text();
+           text=ui->rech_mar->text();
                 if(text == "")
 
                 {
-                    ui->tableView->setModel(P.afficher());
+                    ui->tableView_mar->setModel(P.afficher());
 
                 }
 
                 else
 
                 {
-                    ui->tableView->setModel(P.chercher_offre_nom(text));
+                    ui->tableView_mar->setModel(P.chercher_offre_nom(text));
                 }
 
        }
 
-        else if     (ui->rdate->isChecked()==true)
+        else if     (ui->rdate_mar->isChecked()==true)
         {
 
-            text=ui->rech->text();
+            text=ui->rech_mar->text();
                  if(text == "")
 
                  {
-                     ui->tableView->setModel(P.afficher());
+                     ui->tableView_mar->setModel(P.afficher());
                  }
 
                  else
 
                  {
-                     ui->tableView->setModel(P.chercher_offre_datedebut(text));
+                     ui->tableView_mar->setModel(P.chercher_offre_datedebut(text));
                  }
         }
 }
@@ -454,77 +454,77 @@ void MainWindow::on_recherche_clicked()
 
 
 
-void MainWindow::on_trii_clicked()
+void MainWindow::on_trii_mar_clicked()
 {
 
     evenement o;
        /*QString critere=ui->cb_historique->currentText();*/
            QString mode;
-            if (ui->radioButton->isChecked()==true)
+            if (ui->radioButton_mar->isChecked()==true)
        {
-                ui->tableView_2->setModel(o.trie());
+                ui->tableView_2_mar->setModel(o.trie());
 
 
        }
-            else if(ui->radioButton_2->isChecked()==true)
+            else if(ui->radioButton_2_mar->isChecked()==true)
 
-                ui->tableView_2->setModel(o.trie2());
+                ui->tableView_2_mar->setModel(o.trie2());
 }
 
 
 
-void MainWindow::on_recherchee_clicked()
+void MainWindow::on_recherchee_mar_clicked()
 {
     evenement P;
            QString text;
 
-           if (ui->ridd->isChecked()==true)
+           if (ui->ridd_mar->isChecked()==true)
           {
-          text=ui->ra->text();
+          text=ui->ra_mar->text();
             if(text == "")
             {
-                ui->tableView_2->setModel(P.afficher());
+                ui->tableView_2_mar->setModel(P.afficher());
             }
 
             else
             {
-                ui->tableView_2->setModel(P.chercher_evenement_id(text));
+                ui->tableView_2_mar->setModel(P.chercher_evenement_id(text));
             }
            }
 
 
-          if(ui->nomm->isChecked()==true)
+          if(ui->nomm_mar->isChecked()==true)
            {
-               text=ui->ra->text();
+               text=ui->ra_mar->text();
                     if(text == "")
 
                     {
-                        ui->tableView_2->setModel(P.afficher());
+                        ui->tableView_2_mar->setModel(P.afficher());
 
                     }
 
                     else
 
                     {
-                        ui->tableView_2->setModel(P.chercher_evenement_nom(text));
+                        ui->tableView_2_mar->setModel(P.chercher_evenement_nom(text));
                     }
 
            }
 
-            else if     (ui->datess->isChecked()==true)
+            else if     (ui->datess_mar->isChecked()==true)
             {
 
-                text=ui->ra->text();
+                text=ui->ra_mar->text();
                      if(text == "")
 
                      {
-                         ui->tableView_2->setModel(P.afficher());
+                         ui->tableView_2_mar->setModel(P.afficher());
                      }
 
                      else
 
                      {
-                         ui->tableView_2->setModel(P.chercher_evenement_dates(text));
+                         ui->tableView_2_mar->setModel(P.chercher_evenement_dates(text));
                      }
             }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -18,103 +18,104 @@ public:
 
 private slots:
 
-    void on_buton_ajouter_clicked();
+    void on_buton_ajouter_mar_clicked();
 
-    void on_pushButton_4_clicked();
+    void on_pushButton_4_mar_clicked();
 
-    void on_boutonsupprimer_clicked();
+    void on_boutonsupprimer_mar_clicked();
 
-    void on_bouton_modifier_clicked();
+    void on_bouton_modifier_mar_clicked();
 
-    void on_retour4_clicked();
+    void on_retour4_mar_clicked();
 
-    void on_pushButton_2_clicked();
+    void on_pushButton_2_mar_clicked();
 
-    void on_RETOUR1_clicked();
+    void on_RETOUR1_mar_clicked();
 
-    void on_pushButton_3_clicked();
+    void on_pushButton_3_mar_clicked();
 
-    void on_lineEdit_textChanged(const QString &arg1);
+    void on_lineEdit_mar_textChanged(const QString &arg1);
 
-    void on_pushButton_5_clicked();
+    void on_pushButton_5_mar_clicked();
 
 
 void notif(QString t,QString m);
-    void on_SUPPRIMER_clicked();
+    void on_SUPPRIMER_mar_clicked();
 
-    void on_MODIF_clicked();
+    void on_MODIF_mar_clicked();
 
-    void on_pushButton_6_clicked();
+    void on_pushButton_6_mar_clicked();
 
-    void on_pushButton_8_clicked();
+    void on_pushButton_8_mar_clicked();
 
-    void on_eventsupprimer_2_clicked();
+    void on_eventsupprimer_2_mar_clicked();
 
-    void on_retour4_2_clicked();
+    void on_retour4_2_mar_clicked();
 
-    void on_pushButton_11_clicked();
+    void on_pushButton_11_mar_clicked();
 
-    void on_pushButton_12_clicked();
+    void on_pushButton_12_mar_clicked();
 
-    void on_RETOUR1_3_clicked();
+    void on_RETOUR1_3_mar_clicked();
 
-    void on_retour4_5_clicked();
+    void on_retour4_5_mar_clicked();
 
-    void on_event_ajouter_2_clicked();
+    void on_event_ajouter_2_mar_clicked();
 
-    void on_event_afficher_clicked();
+    void on_event_afficher_mar_clicked();
 
-    void on_event_modifier_2_clicked();
+    void on_event_modifier_2_mar_clicked();
 
-    void on_pushButton_9_clicked();
+    void on_pushButton_9_mar_clicked();
 
-    void on_lineEdit_2_textChanged(const QString &arg1);
+    void on_lineEdit_2_mar_textChanged(const QString &arg1);
 
-    void on_SUPPRIMER_3_clicked();
+    void on_SUPPRIMER_3_mar_clicked();
 
-    void on_pushButton_24_clicked();
+    void on_pushButton_24_mar_clicked();
 
-    void on_pushButton_7_clicked();
-
-
-    void on_pushButton_13_clicked();
+    void on_pushButton_7_mar_clicked();
 
 
-    void on_pushButton_65_clicked();
+    void on_pushButton_13_mar_clicked();
 
-    void on_pushButton_14_clicked();
 
-    void on_retour4_3_clicked();
+    void on_pushButton_65_mar_clicked();
+
+    void on_pushButton_14_mar_clicked();
+
+    void on_retour4_3_mar_clicked();
 
    // void on_lineEdit_adresse_cursorPositionChanged(int arg1, int arg2);
 
-    void on_pushButton_clicked();
+    void on_pushButton_mar_clicked();
 
-    void on_pushButton_10_clicked();
+    void on_pushButton_10_mar_clicked();
 
-    void on_pushButton_15_clicked();
+    void on_pushButton_15_mar_clicked();
 
-    void on_lineEdit_cursorPositionChanged(int arg1, int arg2);
+    void on_lineEdit_mar_cursorPositionChanged(int arg1, int arg2);
 
-    void on_SUPPRIMER_2_clicked();
+    void on_SUPPRIMER_2_mar_clicked();
 
-    void on_SUPPRIMER_4_clicked();
+    void on_SUPPRIMER_4_mar_clicked();
 
-    void on_tableView_2_activated(const QModelIndex &index);
+    void on_tableView_2_mar_activated(const QModelIndex &index);
 
-    void on_pdf_clicked();
+    void on_pdf_mar_clicked();
 
-    void on_tri_clicked();
+    void on_tri_mar_clicked();
 
-    void on_recherche_clicked();
+    void on_recherche_mar_clicked();
 
-    void on_msg_cursorPositionChanged(int arg1, int arg2);
+    void on_msg_mar_cursorPositionChanged(int arg1, int arg2);
 
-    void on_testtest_clicked();
+    void on_testtest_mar_clicked();
 
-    void on_trii_clicked();
+    void on_trii_mar_clicked();
 
-    void on_recherchee_clicked();
+    void on_recherchee_mar_clicked();
+
 
 private:
     Ui::MainWindow *ui;
